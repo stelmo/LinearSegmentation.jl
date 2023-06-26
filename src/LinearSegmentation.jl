4fca099ca7026c38e7f7aa6936410d2e14d42e50
St. Elmo Wilken <stelmozors@gmail.com>
2023-06-26 15:26:07 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.38

---
+++ src/LinearSegmentation.jl
@@ -0,0 +1,5 @@
+module LinearSegmentation
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LinearSegmentation
+using Test
+
+@testset "LinearSegmentation.jl" begin
+    # Write your tests here.
+end
