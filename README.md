8563ed36f7cd0dd7b8741fa8c0d38a39102578b2
St. Elmo Wilken <stelmozors@gmail.com>
2023-06-27 20:36:43 +0200

    fix tests

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # LinearSegmentation
 
 [![Build Status](https://github.com/stelmo/LinearSegmentation.jl/actions/workflows/CI.yml/badge.svg?branch=master)](https://github.com/stelmo/LinearSegmentation.jl/actions/workflows/CI.yml?query=branch%3Amaster)
+
+# Sliding window
+
+# Top down
+
+# Graph segmentation
+
+# Other useful resources
+https://cran.r-project.org/web/packages/dpseg/vignettes/dpseg.html#appi
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,15 @@ using Graphs, GLM
 # test settings
 atol = 1e-6
 
+# settings
+min_segment_length = 1.0
+max_rmse = 0.10
+
+# generate test data
+N = 100
+xs = collect(range(0, 3 * pi, length = N))
+ys = sin.(xs)
+
 @testset "LinearSegmentation.jl" begin
     @testset "Internals" begin
         include("types.jl")
@@ -13,15 +22,6 @@ atol = 1e-6
     end
 
     @testset "Segmentation " begin
-        # settings
-        min_segment_length = 1.0
-        max_rmse = 0.10
-
-        # generate test data
-        N = 100
-        xs = collect(range(0, 3 * pi, length = N))
-        ys = sin.(xs)
-
         include("slidingwindow.jl")
         include("topdown.jl")
         include("graph_segmentation.jl")
